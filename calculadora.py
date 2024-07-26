ae3b5a809fe68b2b2971f05c5d2adf882084ffd1
Matharaujo <matheusaraujopoe@gmail.com>
2024-07-26 10:39:39 -0300

    o cauculo em si

--- calculadora.py
+++ calculadora.py
@@ -8,5 +8,25 @@ def multi(a,b):
     return a*b
 
 def divi(a,b):
-    return a/b
+    if b != 0:
+        return a/b
+    else:
+        return 'não é possivel dividir por zero'
+
+while True:
+    op = input('selecione a operação : +, -, *, / ou sair')
+    if op == 'sair':
+        break
+    a = float(input('digite um numero'))
+    b = float(input('digite um numero'))
+
+    if op == '+':
+        print('Resultado :',adicionar(a,b))
+    elif op == '-':
+        print('Resultado :', substrair(a, b))
+    elif op == '*':
+        print('Resultado :', multi(a, b))
+    elif op == '/':
+        print('Resultado :', divi(a, b))
+
 
